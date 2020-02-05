7c61a90a71aaf3e330814ee4c3c6423e301d91c1
Drew Dickerson <ddickerson@magmutual.com>
2020-02-05 10:49:47 -0500

    Add specs for development

--- spec/konnichiwa_spec.rb
+++ spec/konnichiwa_spec.rb
@@ -3,5 +3,19 @@
 require 'spec_helper'
 
 RSpec.describe 'The Konnichiwa App' do
+  def app
+    Sinatra::Application
+  end
 
+  it 'says hello' do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('こんにちは世界')
+  end
+
+  it 'tells you what Frank says' do
+    get '/frank-says'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('Put this in your pipe and smoke it.')
+  end
 end
